99bc4daa471cb0dcfd17b584ce16fed052c403c6
Christian Panadero <panavtec@gmail.com>
2017-08-03 15:32:13 +0100

    Add logout route and models/

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,8 @@ module.exports = app => {
   app.get('/api/current_user', (req, res) => {
     res.send(req.user)
   })
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
 }
