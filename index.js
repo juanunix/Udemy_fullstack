4527540d267810c6b0e65bc002f153906d9d49ea
Christian Panadero <panavtec@gmail.com>
2017-08-03 12:11:15 +0100

    Move routes and passport configuration

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const express = require('express')
-const app = express()
+require('./services/passport')
 const PORT = process.env.PORT || 5000
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
 
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, accessToken => console.log(accessToken))
-)
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}))
-app.get('/auth/google/callback', passport.authenticate('google'))
-
+const app = express()
+require('./routes/authRoutes')(app)
 app.listen(PORT)
