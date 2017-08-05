d0819fd8da252698de08385d25bd005ea6cb4a13
Christian Panadero <panavtec@gmail.com>
2017-08-05 22:23:40 +0100

    Complete login flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,13 @@ module.exports = app => {
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
   }))
-  app.get('/auth/google/callback', passport.authenticate('google'))
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys')
+    }
+  )
   app.get('/api/current_user', (req, res) => {
     res.send(req.user)
   })
