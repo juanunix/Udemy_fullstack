86850992050341e8add25a34704c4945253ed8ac
Christian Panadero <panavtec@gmail.com>
2017-08-05 22:04:42 +0100

    implement reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,4 +4,4 @@ import { FETCH_USER as type } from './types'
 export const fetchUser = () => dispatch =>
   axios
   .get('/api/current_user')
-  .then(payload => dispatch({ type, payload }))
+  .then(res => dispatch({ type, payload: res.data }))
