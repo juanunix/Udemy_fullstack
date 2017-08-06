c50fa8ec9dcd3d4749c76b8bef442f66fa5a3fb9
Christian Panadero <panavtec@gmail.com>
2017-08-06 15:47:47 +0100

    connect paymets to the reducer and call handleToken

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchUser = () => dispatch =>
   axios
   .get('/api/current_user')
   .then(res => dispatch({ type, payload: res.data }))
+
+export const handleToken = token => dispatch =>
+  axios
+    .post('/api/stripe', token)
+    .then(res => dispatch({ type, payload: res.data }))
